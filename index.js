015c6979094c4281efa12493177ebdfda0063455
Bruna Carvalho <brunacarvalhocardoso@gmail.com>
2022-09-02 14:16:04 -0300

    Questionário

--- index.js
+++ index.js
@@ -1,19 +1,41 @@
 const questions = [
-  'O que aprendi hoje?',
-  'O que me deixou aborrecido? E o que eu poderia fazer para melhorar?',
-  'O que me deixou feliz hoje?',
-  'Quantas pessoas ajudei hoje?'
+  'Qual seu nome?',
+  'Data de nascimento?',
+  'Cidade onde nasceu?',
+  'Cidade onde mora?'
 ]
 
 const ask = (index = 0) => {
-  process.stdout.write(questions[index] + '\n\n\n') //processo de saída padrão
+  process.stdout.write('\n\n' + questions[index] + '\n >. ') //processo de saída padrão
 }
 
 ask()
 
 const answers = []
 
-process.stdin.on(null, data => {
-  process.stdout.write( data.toString().trim() + '\n') // essa função será rodada toda vez que houverem/for inserido dados
+process.stdin.on('data', data => {
+  answers.push(data.toString().trim())
+  if (answers.length < questions.length){
+    ask(answers.length)
+  } else {
   process.exit()
+  }
+})
+
+process.on('exit', () => {
+  console.log(`
+  Dados pessoais:
+  
+  Nome:
+  ${answers[0]}
+  
+  Data de nascimento:
+  ${answers[1]}
+  
+  Cidade onde nasceu:
+  ${answers[2]}
+  
+  Cidade onde mora atualmente:
+  ${answers[3]}
+  `)
 })
